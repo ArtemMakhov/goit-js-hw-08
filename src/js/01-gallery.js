c1134d5f29933b197f45a8ddacece53027020186
Artem Makhov <makhovartem23@gmail.com>
2022-06-11 19:36:31 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const picturesMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+
+function createGalleryMarkup(pictures) {
+    return pictures.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div> 
+    `
+    })
+        .join('');
+};
+
+
+new SimpleLightbox('.gallery a', { captionsData:"alt",captionDelay:250 });
