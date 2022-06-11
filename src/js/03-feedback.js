a6b6cf878fe06ce672dfde28ed0dda7303fc11b0
Artem Makhov <makhovartem23@gmail.com>
2022-06-11 19:24:05 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input:document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onFormIput, 500));
+refs.input.addEventListener('input',throttle(onFormIput,500) );
+
+refs.form.addEventListener('input', e => {
+   
+    formData[e.target.name] = e.target.value;
+});
+
+displayTextInForm();
+
+
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+   
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+
+function onFormIput() {
+     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+
+function displayTextInForm() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    const parsedMessage = JSON.parse(saveMessage);
+    
+    if (parsedMessage) {
+        console.log(parsedMessage);
+        
+        refs.textarea.value = parsedMessage.message;
+        refs.input.value = parsedMessage.email;
+}
+};
